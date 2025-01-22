38c29b601bd378109b2f76f33e39facb520de113
Jeff Carbine <jeffcarbine@gmail.com>
2025-01-21 21:56:17 -0700

    fixed issue with the engine not importing

--- engine.js
+++ engine.js
@@ -6,7 +6,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Modules
-import Materia from "materia";
+import Materia from "@jeffcarbine/materia";
 const materia = new Materia();
 
 /**
